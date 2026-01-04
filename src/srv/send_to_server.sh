fd597ff2a9c49c03cc6e8418de300ef4a788fbac
emianuelul <emanuel.marin0000@gmail.com>
2026-01-04 17:54:28 +0200

    remove ips and remove shell script

--- src/srv/send_to_server.sh
+++ src/srv/send_to_server.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 docker run --rm -v $(pwd)/../../:/work -w /work/src/srv alpine:latest sh -c "
-  apk add --no-cache g++ nlohmann-json sqlite-dev sqlite-static build-base && \
+  apk add --no-cache g++ gcc nlohmann-json sqlite-dev sqlite-static build-base && \
+  gcc -c -I../../include ../../include/aes.c -o aes.o && \
+  chmod 666 aes.o && \
   g++ -std=c++20 -static \
   -I../../include \
   -I/usr/include \
-  server.cpp \
+  server.cpp aes.o \
   -o server_exec \
-  -pthread -lsqlite3
+  -pthread -lsqlite3 && \
+  chmod 777 server_exec && \
+  rm -f aes.o
 " && \
 scp server_exec emanuel.marin@10.100.0.30:~/ && \
 rm server_exec
